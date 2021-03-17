05e441a649ab38935f63412fb16043383c033514
Ho-Huan Chiang <hc57@rice.edu>
2021-03-17 04:13:56 -0500

    change layout of about page

--- hope-simpson-web-app/src/About/About.js
+++ hope-simpson-web-app/src/About/About.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './About.css';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
+import {TimelineLite,gsap,Power3} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 const slideImages = [
   './images/p0.jpg',
   './images/p1.jpg',
@@ -27,66 +30,266 @@ const properties = {
 
 const Slideshow = () => {
   return (
-    <div className={"bodyMarginTop"}>
-      <Slide easing="ease" {...properties}>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[3]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[4]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[5]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[6]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[7]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[8]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[9]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[10]})`}}>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[11]})`}}>
-          </div>
-        </div>
-      </Slide>
+    <div className={"slides"}>
+      <Welcome/>
+      <Inspiration/>
+      <Distribution/>
+      <Proposal/>
+      <SeasonOutBreak/>
     </div>
   )
 };
-
-function About() {
+const SeasonOutBreak=()=>{
   return (
-    <div className="about-container container">
-      <h1 className="text-center">About the Hope-Simpson Concept</h1>
-      <hr/>
+      <div>
+        <div className={"slidesInnerDiv"}>
+          <div className={"title seasonTitle"}>Influenza Outbreaks Follow the Season and Latitude</div>
+          <div className={"seasonContent"}>
+            <img src={"/images/latitudeCategory.png"} className={"latitudeImage"}/>
+            <div className={"seasonText"}>
+              <div>
+                The spike in influenza cases occurs in January in the north temperate zone
+              </div>
+              <div>
+                A more moderated distribution of influenza cases nearer the equator, in the north and south tropical zones
+              </div>
+              <div>
+                The spike in influenza cases, six months later, occurs in July in the south temperate zone
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+const Proposal=()=>{
+  return (
+      <div>
+        <div className={"slidesInnerDiv"}>
+          <div className={"title proposalTitle"}>Proposal of a Seasonally Mediated Stimulus</div>
+          <div className={"proposalContent"}>
+            <img src={"/images/latitudeCategory.png"} className={"latitudeImage"}/>
+            <div class={"proposalText"}>
+              <ul>
+                <li>
+                  From The Transmission of Epidemic Influenza
+                  <ul>
+                    <li>PROPOSITION 3: CONCERNING SEASONAL REACTIVATION OF PERSISTENT INFLUENZA VIRUS</li>
+                    <ul>
+                      <li>The persistent influenza virus infecting human carriers is annually reactivated to infectiousness by a seasonally mediated stimulus. The carrier, usually without again falling ill, becomes highly infectious for a brief period and his nonimmune companions, if infected, rapidly develop an attack of influenza.” (p. 193)</li>
+                    </ul>
+                  </ul>
+                </li>
+                <li>“The conclusion seems inescapable that, viewed on a global scale, epidemic influenza is moving annually south and then north through the world population, a smooth yearly scanning of the world very different from the local episodic picture of the disease.”</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+const Distribution=()=>{
+  return (
+      <div>
+        <div className={"slidesInnerDiv"}>
+          <div className={"title distributeTitle"}>Distribution of Influenza Outbreaks by Latitude Throughout The Year</div>
+          <div className={"distributeContent"}>
+            <div className={"distributeText"}>
+              <div className={"distributeInnerText"}>
+                <ul>
+                  <li>
+                    Hope-Simpson divided the world into four latitudinal zones
+                    <ul>
+                      <li>North Temperate Zone: Latitudes at more than 30o N</li>
+                      <li>North Tropical Zone: Latitudes from the equator to 30o N</li>
+                      <li>South Tropical Zone: Latitudes from the equator to 30o S</li>
+                      <li>South Tropical Zone: Latitudes at more than 30o S</li>
+                    </ul>
+                  </li>
+                  <li>For each zone of latitude, he showed the distribution (histogram) of influenza cases by month</li>
+                  <li>
+                      He overlayed the “Path of Vertical Solar Radiation”
+                      <ul>
+                        <li>Due to the tilt of the earth’s axis of rotation, the path of vertical solar radiation sweeps from the most northern point (23.5o N) to the most southern point (23.5o S)</li>
+                      </ul>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <img src={"/images/tiltEarth.png"} className={"tiltEarthImage"}/>
+              </div>
+            </div>
+            <img src={"/images/latitudeCategory.png"} className={"latitudeImage"}/>
+
+          </div>
+        </div>
+      </div>
+
+  )
+
+}
+const Welcome=()=>{
+  return (
+      <div>
+        <div className={"slidesInnerDiv"}>
+          <div className={"title welcomeTitle"}>Welcome</div>
+          <div>
+            <div className={"welcomeText"}>
+              The Hope-Simpson Map project (HS) started by students from the fall 2020 class on advanced computer
+              architecture.
+              <div>
+                <ul>
+                  <li>Computer Science majors</li>
+                  <li>Electrical and Computer Engineering majors</li>
+                </ul>
+              </div>
+            </div>
+            <div className={"myLine"}></div>
+            <div>
+              <div style={{textAlign: "center"}}>We created a web platform based on</div>
+              <div className={"welcomeImages"}>
+                <div className={"welcomeImages1"}>
+                  <img src={"/images/database.png"}/>
+                  <span>Modern databases</span>
+                </div>
+                <div className={"welcomeImages2"}>
+                  <img src={"/images/visualization.png"}/>
+                  <span>Data visualization</span>
+                </div>
+                <div className={"welcomeImages3"}>
+                  <img src={"/images/web.png"}/>
+                  <span>Web design</span>
+                </div>
+                <div className={"welcomeImages4"}>
+                  <img src={"/images/machine.png"}/>
+                  <span>Virtual machines</span>
+                </div>
+                <div className={"welcomeImages5"}>
+                  <img src={"/images/signal.png"}/>
+                  <span>Signal processing</span>
+                </div>
+                <div className={"welcomeImages6"}>
+                  <img src={"/images/data.png"}/>
+                  <span>Data analysis</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+const Inspiration=()=>{
+  return (
+      <div>
+        <div className={"slidesInnerDiv"}>
+          <div className={"title inspirationTitle"}>Inspiration</div>
+          <div className={"inspirationDiv"}>
+            <img src={"/images/BookCover.png"} className={"bookCoverImage"}/>
+            <div className={"inspirationText"}>
+                <ul>
+                  <li>
+                    Robert Edgar Hope-Simpson (1908-2003) was a general practitioner.
+                    <ul>
+                      <li>
+                        He showed that shingles was caused by reactivation of the chickenpox virus.
+                      </li>
+                    </ul>
+                  </li>
+                  <li>The influenza epidemic of 1932-33,  the year he began his practice, motivated him to study the influenza virus.</li>
+                  <li>
+                    60 years of extensive historical research culminated in the publication of his book The Transmission of Epidemic Influenza
+                    <ul>
+                      <li>Includes Houston influenza data</li>
+                    </ul>
+                  </li>
+                  <li>Proposed that person-to-person transmission was insufficient to explain the pattern of influenza outbreaks around the world.</li>
+                  <li>Proposed an influential seasonally-mediated mechanism.</li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  )
+}
+function About() {
+  let t1=new  TimelineLite({delay: 0.3});
+  useEffect(()=>{
+    //t1.from('.mainTitle',{y:15,opacity:0,ease: Power3.easeIn,delay:0.1},'Start');
+      t1.from('.welcomeImages1',{opacity:0,x:-100,duration:1,ease: Power3.easeIn,delay:0.1},'Start');
+      t1.from('.welcomeImages2',{opacity:0,x:-100,duration:1,ease: Power3.easeIn,delay:0.1},'Start');
+      t1.from('.welcomeImages3',{opacity:0,x:-100,duration:1,ease: Power3.easeIn,delay:0.1},'Start');
+      t1.from('.welcomeImages4',{opacity:0,x:100,duration:1,ease: Power3.easeIn,delay:0.1},'Start');
+      t1.from('.welcomeImages5',{opacity:0,x:100,duration:1,ease: Power3.easeIn,delay:0.1},'Start');
+      t1.from('.welcomeImages6',{opacity:0,x:100,duration:1,ease: Power3.easeIn,delay:0.1},'Start');
+    gsap.from('.welcomeText',{opacity:0,y:100,duration:1});
+    gsap.from('.welcomeTitle',{opacity:0,y:100,duration:1});
+
+      /*
+    gsap.from('.welcomeImages',{opacity:0,x:-100,duration:1,scrollTrigger:{
+        trigger:'.welcomeImages',
+        start: "center 80%"
+      }});*/
+    gsap.from('.bookCoverImage',{opacity:0,x:-100,duration:1,scrollTrigger:{
+        trigger:'.bookCoverImage',
+        start: "top 80%"
+      }});
+    gsap.from('.inspirationText',{opacity:0,x:100,duration:1,scrollTrigger:{
+        trigger:'.inspirationText',
+        start: "top 80%"
+      }});
+    gsap.from('.inspirationTitle',{opacity:0,y:100,duration:1,scrollTrigger:{
+        trigger:'.inspirationTitle',
+        start: "top 80%"
+      }});
+    gsap.from('.distributeTitle',{opacity:0,y:-100,duration:1,scrollTrigger:{
+        trigger:'.distributeTitle',
+        start: "top 80%"
+      }});
+    gsap.from('.distributeInnerText',{opacity:0,x:-100,duration:1,scrollTrigger:{
+        trigger:'.distributeInnerText',
+        start: "top 80%"
+      }});
+    gsap.from('.tiltEarthImage',{opacity:0,y:100,duration:1,scrollTrigger:{
+        trigger:'.tiltEarthImage',
+        start: "top 80%"
+      }});
+      gsap.from('.distributeContent .latitudeImage',{opacity:0,x:400,duration:1,rotation: 180,scrollTrigger:{
+              trigger:'.distributeContent .latitudeImage',
+              start: "top 80%"
+          }});
+    gsap.from('.proposalTitle',{opacity:0,y:100,duration:1,scrollTrigger:{
+        trigger:'.proposalTitle',
+        start: "top 80%"
+      }});
+    gsap.from('.proposalText',{opacity:0,x:100,duration:1,scrollTrigger:{
+        trigger:'.proposalText',
+        start: "top 80%"
+      }});
+    /*
+    gsap.from('.proposalContent .latitudeImage',{opacity:0,x:-100,duration:1,scrollTrigger:{
+        trigger:'.proposalContent .latitudeImage',
+        start: "center 80%"
+      }});
+*/
+    gsap.from('.seasonTitle',{opacity:0,y:100,duration:1,scrollTrigger:{
+        trigger:'.seasonTitle',
+        start: "center 80%"
+      }});
+    gsap.from('.seasonText',{opacity:0,x:100,duration:1,scrollTrigger:{
+        trigger:'.seasonText',
+        start: "center 80%"
+      }});
+    gsap.from('.seasonContent .latitudeImage',{opacity:0,x:-100,duration:1,scrollTrigger:{
+        trigger:'.seasonContent .latitudeImage',
+        start: "center 80%"
+      }});
+
+  })
+  return (
+    <div className="bodyMarginTop">
+      <div className="text-center mainTitle">About the Hope-Simpson Concept</div>
       { Slideshow() }
     </div>
   );
--- hope-simpson-web-app/src/Home/Home.js
+++ hope-simpson-web-app/src/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Home.css';
 import TableauDashboard from "../TableauDashboard/TableauDashboard";
 
@@ -7,81 +7,90 @@ const TABLEAU_WORLD_MAP_DASHBOARD_URL = 'https://public.tableau.com/views/hs-wor
 const TABLEAU_OPTIONS = { device: ["desktop", "phone"] };
 
 
-function Home() {
+class Home extends Component{
+    constructor() {
+        super();
+        this.state={
+            smallScreen: false,
+            iframeURL:"https://datastudio.google.com/embed/reporting/b5f88341-c05e-4114-a695-1923b4cd7bd2/page/TxgjB"
+        }
+    }
+    componentDidMount() {
+        window.addEventListener("resize", this.resize.bind(this));
+        this.resize();
+    }
 
-  return (
-    <div className={"home-container bodyMarginTop"}>
-        <div className={"homeTitles"}>
-          <div className="text-center mainTitle">Exploring COVID-19 Cases Around the World</div>
-          <div className="text-center subTitle">Modern Tools Applied to a 90’s Concept</div>
-        </div>
-        <div id={"section1"}>
-            <div className={"title"}>Introduction</div>
-            <div className="lead">A very cordial welcome to the Hope-Simpson project started by a group of students from the fall 2020 class of COMP/ELEC 425/554! We created a platform based on modern databases, visualization, web design, virtual machines, signal processing, and data analysis. We analyzed COVID-19 data on our platform based on Robert Edgar Hope-Simpson's perspective and his study of the spread of the influenza virus.</div>
-            <br/>
-            <div className="lead">On our home page, you can begin to explore COVID-19 cases around the world. The histogram displays confirmed cases in weekly increments based on the locations you selected.</div>
-        </div>
-      <div className={"homeTableau"}>
-          <div className={"caseDetails"}>
-              <div>
-                  <div>Global Confirmed</div>
-                  <div>117,556,326</div>
-              </div>
-              <div>
-                  <div>Global Deaths</div>
-                  <div>2,610,135</div>
-              </div>
-              <div>
-                  <div>U.S. Confirmed</div>
-                  <div>29,094,540</div>
-              </div>
-              <div>
-                  <div>U.S. Deaths</div>
-                  <div>527,643</div>
-              </div>
-          </div>
-          <div className={"tableauGraph"}>
-        <TableauDashboard
-          url={ TABLEAU_WORLD_MAP_DASHBOARD_URL }
-          options={ TABLEAU_OPTIONS }
-        />
-          </div>
-      </div>
-        <div id={"section2"}>
-            <div>
-                <p className="lead">Let us take you through a journey of ways to look at the current worldwide pandemic through the lens of Hope-Simpson. Here is what you can find on our other pages:</p>
-                <div className={"homeCards"}>
-                    <div className={"cards"}>
-                        <img src={'/assets/about.png'} style={{backgroundColor: "#143642"}}/>
-                        <div className={"cardTitle"}>About</div>
-                        <div className={"cardContent"}>Information on the background inspiration, which is the life work of Robert Edgar Hope-Simpson (1908-2003), a general practitioner who studied influenza cases around the world for sixty years. Also, an explanation of the Hope-Simpson concept and about our COVID-19 data.</div>
+    resize() {
+        console.log(window.innerWidth);
+        if(window.innerWidth>1000){
+            this.setState({...this.state,smallScreen: false,iframeURL:"https://datastudio.google.com/embed/reporting/b5f88341-c05e-4114-a695-1923b4cd7bd2/page/TxgjB"});
+        }
+        else{
+            this.setState({...this.state,smallScreen: true,iframeURL:"https://datastudio.google.com/embed/u/0/reporting/5aa0f5f5-2bf9-4654-8b36-7b6df6dc3d31/page/TxgjB"});
+        }
+        //this.setState({hideNav: window.innerWidth <= 760});
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.resize.bind(this));
+    }
+
+    render() {
+        return (
+            <div className={"home-container bodyMarginTop"}>
+                <div className={"homeTitles"}>
+                    <div className="text-center mainTitle">Exploring COVID-19 Cases Around the World</div>
+                    <div className="text-center subTitle">Modern Tools Applied to a 90’s Concept</div>
+                </div>
+                <div id={"section1"}>
+                    <div className={"title"}>Introduction</div>
+                    <div className="lead">A very cordial welcome to the Hope-Simpson project started by a group of students from the fall 2020 class of COMP/ELEC 425/554! We created a platform based on modern databases, visualization, web design, virtual machines, signal processing, and data analysis. We analyzed COVID-19 data on our platform based on Robert Edgar Hope-Simpson's perspective and his study of the spread of the influenza virus.</div>
+                    <br/>
+                    <div className="lead">On our home page, you can begin to explore COVID-19 cases around the world. The histogram displays confirmed cases in weekly increments based on the locations you selected.</div>
+                </div>
+                <div className={"homeTableau"}>
+                    <div style={{paddingBottom:this.state.smallScreen?"75%":"45%"}}>
+                        <iframe scrolling="no" src={this.state.iframeURL}></iframe>
                     </div>
-                    <div className={"cards"}>
-                        <img src={'/assets/tableau.png'} style={{backgroundColor: "#0F8B8D"}}/>
-                        <div className={"cardTitle"}>Tableau Map</div>
-                        <div className={"cardContent"}>Using Tableau, you can take a Hope-Simpson inspired look at COVID-19 through a sample of four latitude zones around the world. We compared and contrasted different regions and showed a north-to-south longitudinal slice of the Americas.</div>
+                </div>
+                <div id={"section2"}>
+                    <div>
+                        <p className="lead">Let us take you through a journey of ways to look at the current worldwide pandemic through the lens of Hope-Simpson. Here is what you can find on our other pages:</p>
+                        <div className={"homeCards"}>
+                            <div className={"cards"}>
+                                <img src={'/assets/about.png'} style={{backgroundColor: "#143642"}}/>
+                                <div className={"cardTitle"}>About</div>
+                                <div className={"cardContent"}>Information on the background inspiration, which is the life work of Robert Edgar Hope-Simpson (1908-2003), a general practitioner who studied influenza cases around the world for sixty years. Also, an explanation of the Hope-Simpson concept and about our COVID-19 data.</div>
+                            </div>
+                            <div className={"cards"}>
+                                <img src={'/assets/tableau.png'} style={{backgroundColor: "#0F8B8D"}}/>
+                                <div className={"cardTitle"}>Tableau Map</div>
+                                <div className={"cardContent"}>Using Tableau, you can take a Hope-Simpson inspired look at COVID-19 through a sample of four latitude zones around the world. We compared and contrasted different regions and showed a north-to-south longitudinal slice of the Americas.</div>
+                            </div>
+                            <div className={"cards"}>
+                                <img src={'/assets/map.png'} style={{backgroundColor: "#EC9A29"}}/>
+                                <div className={"cardTitle"}>GDS Map</div>
+                                <div className={"cardContent"}>Using Google Data Studio, you can dive into a detailed exploration of the data by date, country, latitude, and longitude. We can see another view of the four-zones concept and the longitudinal slice of the Americas.</div>
+                            </div>
+                            <div className={"cards"}>
+                                <img src={'/assets/modeling.png'} style={{backgroundColor: "#A8201A"}}/>
+                                <div className={"cardTitle"}>Modeling</div>
+                                <div className={"cardContent"}>See early work of a compartmental network model of the virus spread and a Fourier-based analysis of the reported cases across the world.</div>
+                            </div>
+                        </div>
                     </div>
-                    <div className={"cards"}>
-                        <img src={'/assets/map.png'} style={{backgroundColor: "#EC9A29"}}/>
-                        <div className={"cardTitle"}>GDS Map</div>
-                        <div className={"cardContent"}>Using Google Data Studio, you can dive into a detailed exploration of the data by date, country, latitude, and longitude. We can see another view of the four-zones concept and the longitudinal slice of the Americas.</div>
-                    </div>
-                    <div className={"cards"}>
-                        <img src={'/assets/modeling.png'} style={{backgroundColor: "#A8201A"}}/>
-                        <div className={"cardTitle"}>Modeling</div>
-                        <div className={"cardContent"}>See early work of a compartmental network model of the virus spread and a Fourier-based analysis of the reported cases across the world.</div>
+                </div>
+                <div id={"section3"}>
+                    <div>
+                        <div>Finally, our site is constantly redesigned so it might be a bit unresponsive from time to time.  But one of our great design features is that you can download any of the data yourself to continue your own exploration.</div>
+                        <div>Stay safe!</div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div id={"section3"}>
-            <div>
-                <div>Finally, our site is constantly redesigned so it might be a bit unresponsive from time to time.  But one of our great design features is that you can download any of the data yourself to continue your own exploration.</div>
-                <div>Stay safe!</div>
-            </div>
-        </div>
-    </div>
-  );
+        );
+    }
+
+
 }
 
 export default Home;
