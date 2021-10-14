930ce0abcf7b77e10abe8ea9e8dbdbe4595635c4
zzzzzz <chrisz881711@gmail.com>
2021-10-14 10:24:05 -0500

    add captures

--- hope-simpson-web-app/src/Propositions/Proposition.js
+++ hope-simpson-web-app/src/Propositions/Proposition.js
@@ -2,15 +2,23 @@ import React from 'react';
 import './Propositions.css';
 
 const Proposition = (props) => {
-  const { title, description, images } = props;
+  const { title, description, images, subtitles } = props;
   return (
     <div className={'propositionDiv'}>
       {/*<h2 className={"title"}>{title}</h2>*/}
       <p>{description}</p>
       {images.map((image, idx) => {
+        // return (
+        //   <div>
+        //     <img className={'img-fluid'} key={idx} src={require('' + image)} />
+        //   </div>
+        // )
         return (
-          <div>
-            {/*<img className={'img-fluid'} key={idx} src={image}/>*/}
+          <div className="img-text-wrapper">
+            <div className='subtitle' key={idx}>{image.title}</div>
+            <div className="logo-wrapper">
+              <img className={'img-fluid'} key={idx} src={require('' + image.src)} />
+            </div>
           </div>
         )
       })}
--- hope-simpson-web-app/src/Propositions/propositionsDatabse.js
+++ hope-simpson-web-app/src/Propositions/propositionsDatabse.js
@@ -2,27 +2,39 @@ const propositions = [
   {
     title: 'Propositions1',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    images: ['https://picsum.photos/200', 'https://picsum.photos/200', 'https://picsum.photos/200', 'https://picsum.photos/200', 'https://picsum.photos/200', 'https://picsum.photos/200', 'https://picsum.photos/200', 'https://picsum.photos/200', 'https://picsum.photos/200']
+    images: [{ title: 'Italy - Lat: 41.9, Long: 12.6', src: './captures/Italy.png' }],
   },
   {
     title: 'Propositions2',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    images: ['https://picsum.photos/200', 'https://picsum.photos/200']
+    images: [
+      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+    ],
   },
   {
     title: 'Propositions3',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    images: ['https://picsum.photos/200', 'https://picsum.photos/200']
+    images: [
+      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+    ],
   },
   {
     title: 'Propositions4',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    images: ['https://picsum.photos/200', 'https://picsum.photos/200']
+    images: [
+      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+    ],
   },
   {
     title: 'Propositions5',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    images: ['https://picsum.photos/200', 'https://picsum.photos/200']
+    images: [
+      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+    ],
   },
 ];
 
