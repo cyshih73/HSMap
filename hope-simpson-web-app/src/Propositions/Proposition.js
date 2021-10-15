eafd4773c16ccbc5c4a4c3412fa370e63aa2845a
zzzzzz <chrisz881711@gmail.com>
2021-10-14 21:54:04 -0500

    first draft for 9 images

--- hope-simpson-web-app/src/Propositions/Proposition.js
+++ hope-simpson-web-app/src/Propositions/Proposition.js
@@ -6,7 +6,7 @@ const Proposition = (props) => {
   return (
     <div className={'propositionDiv'}>
       {/*<h2 className={"title"}>{title}</h2>*/}
-      <p>{description}</p>
+      {/* <p>{description}</p> */}
       {images.map((image, idx) => {
         // return (
         //   <div>
@@ -14,12 +14,16 @@ const Proposition = (props) => {
         //   </div>
         // )
         return (
-          <div className="img-text-wrapper">
-            <div className='subtitle' key={idx}>{image.title}</div>
-            <div className="logo-wrapper">
-              <img className={'img-fluid'} key={idx} src={require('' + image.src)} />
-            </div>
-          </div>
+          // <div className="img-text-wrapper">
+          //   <div className='image_subtitle' key={idx}>{image.title}</div>
+          //   <div className="logo-wrapper">
+          //     <img className={'img-fluid'} key={idx} src={require('' + image.src)} />
+          //   </div>
+          // </div>
+          <figure className="position-relative">
+            <figcaption className={'image-subtitles'} key={idx}>{image.title}</figcaption>
+            <img className={'img-thumbnail'} key={idx} src={require('' + image.src)} />
+          </figure>
         )
       })}
     </div>
--- hope-simpson-web-app/src/Propositions/propositionsDatabse.js
+++ hope-simpson-web-app/src/Propositions/propositionsDatabse.js
@@ -2,38 +2,48 @@ const propositions = [
   {
     title: 'Propositions1',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    images: [{ title: 'Italy - Lat: 41.9, Long: 12.6', src: './captures/Italy.png' }],
+    images: [
+      { title: 'Italy - Lat: 41.9, Long: 12.6', src: './captures/prop1/Italy.png' },
+      { title: 'Italy - Lat: 41.9, Long: 12.6', src: './captures/prop1/Italy.png' },
+      { title: 'Italy - Lat: 41.9, Long: 12.6', src: './captures/prop1/Italy.png' },
+      { title: 'New York - Lat: 43.0, Long: -75.0', src: './captures/prop1/NewYork.png' },
+      { title: 'New York - Lat: 43.0, Long: -75.0', src: './captures/prop1/NewYork.png' },
+      { title: 'New York - Lat: 43.0, Long: -75.0', src: './captures/prop1/NewYork.png' },
+      { title: 'Texas - Lat: 31.0, Long: -100.0', src: './captures/prop1/Texas.png' },
+      { title: 'Texas - Lat: 31.0, Long: -100.0', src: './captures/prop1/Texas.png' },
+      { title: 'Texas - Lat: 31.0, Long: -100.0', src: './captures/prop1/Texas.png' }
+    ],
   },
   {
     title: 'Propositions2',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     images: [
-      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
-      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/prop1/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/prop1/Italy.png' },
     ],
   },
   {
     title: 'Propositions3',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     images: [
-      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
-      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/prop1/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/prop1/Italy.png' },
     ],
   },
   {
     title: 'Propositions4',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     images: [
-      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
-      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/prop1/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/prop1/Italy.png' },
     ],
   },
   {
     title: 'Propositions5',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     images: [
-      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
-      { title: 'Lat: , Long: ', src: './captures/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/prop1/Italy.png' },
+      { title: 'Lat: , Long: ', src: './captures/prop1/Italy.png' },
     ],
   },
 ];
